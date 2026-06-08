4f4868fe2ba1f78c731f0c8bb936ea9cef03be89
Ana Margarida Vieira <1240811@isep.ipp.pt>
2026-06-08 22:58:10 +0100

    Criação do ficheiro config.php

---
+++ Frontend/config/config.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined('BASE_URL')) define('BASE_URL', '/Projeto SIBDAS');
+if (!defined('APP_NAME')) define('APP_NAME', 'MEDInvenTEC');
+if (!defined('APP_VERSION')) define('APP_VERSION', '1.0.0');
+if (!defined('APP_COPYRIGHT')) define('APP_COPYRIGHT', '© 2026 MEDInvenTEC');

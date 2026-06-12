16e02f1f08374cd48a5cc2fe4cc040acdff7fb2b
Ana Margarida Vieira <1240811@isep.ipp.pt>
2026-06-12 11:30:36 +0100

    Ligação à minha base de dados

--- Frontend/config/config.php
+++ Frontend/config/config.php
@@ -3,3 +3,11 @@ if (!defined('BASE_URL')) define('BASE_URL', '/ProjetoSIBDAS');
 if (!defined('APP_NAME')) define('APP_NAME', 'MEDInvenTEC');
 if (!defined('APP_VERSION')) define('APP_VERSION', '1.0.0');
 if (!defined('APP_COPYRIGHT')) define('APP_COPYRIGHT', '© 2026 MEDInvenTEC');
+
+
+//Base de dados
+define('MYSQL_HOST', 'vsgate-s1.dei.isep.ipp.pt');
+define('MYSQL_PORT', 10464);
+define('MYSQL_DATABASE', 'db1240811');
+define('MYSQL_USERNAME', '1240811');
+define('MYSQL_PASSWORD', 'vieira_811');

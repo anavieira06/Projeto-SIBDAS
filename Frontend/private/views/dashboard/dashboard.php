e18fa948e43d2a8de855e98a50edb039ae85074d
Ana Margarida Vieira <1240811@isep.ipp.pt>
2026-06-12 11:04:55 +0100

    Redireciona todas as páginas para o login quando não autenticado

--- Frontend/private/views/dashboard/dashboard.php
+++ Frontend/private/views/dashboard/dashboard.php
@@ -1,6 +1,15 @@
-<?php include '../../includes/header.php'; ?>
-
 <?php
+// --------------------------------------------------------------------
+// SEGURANÇA: Proteção de acesso à página de edição
+// Este ficheiro deve ser acedido apenas por utilizadores autenticados.
+// Caso não exista sessão iniciada, o utilizador será redirecionado para o login.
+// --------------------------------------------------------------------
+require_once __DIR__ . '/../../includes/funcoes.php';
+redirect_if_not_logged(); // Inicia a sessão (se necessário) e verifica se o utilizador está autenticado
+
+
+include '../../includes/header.php'; 
+
 $pagina = 'normal';
 include '../../includes/nav.php';
 include '../../includes/sidebar.php';
